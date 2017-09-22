15a5ef410529be0b3f1cbb62f75e894932b71b5e
Nicholas Hurley <hurley@mozilla.com>
2017-09-22 14:31:32 -0700

    Try running firefox unit tests with a known-good node

--- run-firefox-tests.sh
+++ run-firefox-tests.sh
@@ -9,6 +9,5 @@ MOZ_NODE_PATH="$(which node)"
 export MOZ_NODE_PATH
 
 cd firefox-tests
-./mach --help
-#./mach xpcshell-test xpcshell/tests/netwerk/test/unit/test_http2.js
+./mach xpcshell-test xpcshell/tests/netwerk/test/unit/test_http2.js
 exit $?
