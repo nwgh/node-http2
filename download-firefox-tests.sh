cdbba228b960ca2b0b167598e773e144fe019a2b
Nicholas Hurley <hurley@mozilla.com>
2017-09-22 14:36:58 -0700

    also download mozharness

--- download-firefox-tests.sh
+++ download-firefox-tests.sh
@@ -21,9 +21,14 @@ esac
 FX_VERSION="$(curl https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/config/version.txt)"
 TEST_INFO="$(curl https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-${FX_VERSION}.en-US.${FX_PLATFORM}.test_packages.json)"
 TEST_FILES_COUNT="$(echo $TEST_INFO | jq '.xpcshell | length')"
+
 mkdir firefox-tests
+
 for i in $(seq 0 $(($TEST_FILES_COUNT - 1))) ; do
     TEST_FILE="$(echo $TEST_INFO | jq -r ".xpcshell[$i]")"
     curl -o "$TEST_FILE" "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/${TEST_FILE}"
     unzip -q -u -d firefox-tests "$TEST_FILE"
 done
+
+curl -o mozharness.zip https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/mozharness.zip
+unzip -q -u -d firefox-tests mozharness.zip
