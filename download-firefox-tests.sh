c83484f5beb24d0c1f14b474714ef8087751667e
Nicholas Hurley <hurley@mozilla.com>
2017-09-22 15:05:24 -0700

    Use mozharness to do most of the setup

--- download-firefox-tests.sh
+++ download-firefox-tests.sh
@@ -3,14 +3,10 @@
 set -ex
 
 OS="$(uname -s)"
-FX_PLATFORM=""
+FX_PLATFORM="linux-x86_64"
 case "$OS" in
-    "Darwin")
-        FX_PLATFORM="mac"
-        ;;
     "Linux")
-        # TODO - figure out cpu type here, too
-        FX_PLATFORM="linux-x86_64"
+        # Just take this for granted that this is what we're using now
         ;;
     *)
         echo "Unknown test platform."
@@ -19,16 +15,21 @@ case "$OS" in
 esac
 
 FX_VERSION="$(curl https://hg.mozilla.org/mozilla-central/raw-file/tip/browser/config/version.txt)"
-TEST_INFO="$(curl https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-${FX_VERSION}.en-US.${FX_PLATFORM}.test_packages.json)"
-TEST_FILES_COUNT="$(echo $TEST_INFO | jq '.xpcshell | length')"
+TEST_PACKAGES="https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-${FX_VERSION}.en-US.${FX_PLATFORM}.test_packages.json"
+INSTALLER="https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/firefox-${FX_VERSION}.en-US.${FX_PLATFORM}.tar.bz2"
 
 mkdir firefox-tests
 
-for i in $(seq 0 $(($TEST_FILES_COUNT - 1))) ; do
-    TEST_FILE="$(echo $TEST_INFO | jq -r ".xpcshell[$i]")"
-    curl -o "$TEST_FILE" "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/${TEST_FILE}"
-    unzip -q -u -d firefox-tests "$TEST_FILE"
-done
-
 curl -o mozharness.zip https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/mozharness.zip
 unzip -q -u -d firefox-tests mozharness.zip
+
+cd firefox-tests
+TEST_ROOT="$(pwd)"
+
+# Let mozharness do most of the work for us, since it'll set stuff up right
+"${TEST_ROOT}/mozharness/scripts/desktop_unittest.py" --config-file "${TEST_ROOT}/mozharness/configs/unittests/linux_unittest.py" --config-file "${TEST_ROOT}/mozharness/configs/remove_executables.py" --no-read-buildbot-config --installer-url="${INSTALLER}" --test-packages-url="${TEST_PACKAGES}" --xpcshell-suite=xpcshell --total-chunk=1 --this-chunk=1 --download-symbols=ondemand --no-run-tests
+
+# Now a bit more setup for goodness/ease of use
+mkdir "${HOME}/bin"
+ln -s "${TEST_ROOT}/build/tests/mach" "${HOME}/bin"
+ln "${TEST_ROOT}/build/venv/bin/python" "${TEST_ROOT}/build/venv/bin/python2.7"
--- run-firefox-tests.sh
+++ run-firefox-tests.sh
@@ -8,6 +8,11 @@ export NODE_HTTP2_ROOT
 MOZ_NODE_PATH="$(which node)"
 export MOZ_NODE_PATH
 
+PATH="${PATH}:${HOME}/bin"
+export PATH
+
 cd firefox-tests
-./mach xpcshell-test xpcshell/tests/netwerk/test/unit/test_http2.js
+TEST_ROOT="$(pwd)"
+source "${TEST_ROOT}/build/venv/bin/activate"
+mach xpcshell-test xpcshell/tests/netwerk/test/unit/test_http2.js
 exit $?
