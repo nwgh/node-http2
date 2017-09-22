91484403bd44857a44dfd946d3c04d4584e56118
Nicholas Hurley <hurley@mozilla.com>
2017-09-22 13:46:26 -0700

    Make downloading firefox tests a bit quieter

--- download-firefox-tests.sh
+++ download-firefox-tests.sh
@@ -25,5 +25,5 @@ mkdir firefox-tests
 for i in $(seq 0 $(($TEST_FILES_COUNT - 1))) ; do
     TEST_FILE="$(echo $TEST_INFO | jq -r ".xpcshell[$i]")"
     curl -o "$TEST_FILE" "https://ftp.mozilla.org/pub/firefox/nightly/latest-mozilla-central/${TEST_FILE}"
-    unzip -u -d firefox-tests "$TEST_FILE"
+    unzip -q -u -d firefox-tests "$TEST_FILE"
 done
